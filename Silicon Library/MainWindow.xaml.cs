78ab8c6f63e264811901014f39ea51ad801e9206
NoelBoi <thenoelboi@gmail.com>
2023-07-11 19:12:18 +0530

    Trimmed Repo and added Visual Polish

--- Silicon Library/MainWindow.xaml.cs
+++ Silicon Library/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Silicon_Library.Helpers;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Silicon_Library.Helpers;
 
 namespace Silicon_Library;
 
@@ -11,5 +13,17 @@ public sealed partial class MainWindow : WindowEx
         AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/WindowIcon.ico"));
         Content = null;
         Title = "AppDisplayName".GetLocalized();
+
+        this.InitializeComponent();
+        var root = this.Content as FrameworkElement;
+        if (root != null)
+            root.Loaded += async (s, e) => await DisplayDialog();
+    }
+    private async Task DisplayDialog()
+    {
+        var dlg = new ContentDialog();
+        dlg.XamlRoot = this.Content.XamlRoot;
+        dlg.Content = "Hello World";
+        await dlg.ShowAsync();
     }
 }
